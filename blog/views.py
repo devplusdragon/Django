79f4dff2797e2055c42b40a78a20f00b8d0b1526
dragongylee <dragongylee@naver.com>
2022-02-19 18:43:15 +0900

    FVC블로그 포스트 목록 페이지 만들기 완료3 2022/02/19

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/post_list.html'
+    ordering = '-pk'
+
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
-
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+#
+#
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
