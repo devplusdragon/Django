789abe869f2edafac3c00d149cf510d20e383f4a
dragongylee <dragongylee@naver.com>
2022-02-23 11:41:22 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가하기  2022/02/23

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,17 @@ import os
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -15,7 +26,7 @@ class Category(models.Model):
         return f'/blog/category/{self.slug}/'
 
     class Meta:
-        verbose_name_plural='Categories'
+        verbose_name_plural = 'Categories'
 
 
 class Post(models.Model):
@@ -30,7 +41,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey(Category, null=True, blank=True,  on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag,  blank=True)
+
     # author:추후 작성 예정
 
     def __str__(self):
